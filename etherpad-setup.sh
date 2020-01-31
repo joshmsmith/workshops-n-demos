c4a7e0e89a560329e462bf554fcbd7409f43698d
Joshua Smith <joshmsmith@gmail.com>
2020-01-30 22:32:06 -0500

    adding etherpad setup script

---
+++ etherpad-setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash 
+
+# author : josh smith 
+
+
+oc new-project etherpad --display-name "Shared Etherpad"
+
+oc new-app mysql-persistent --param MYSQL_USER=ether --param MYSQL_PASSWORD=ether --param MYSQL_DATABASE=ether --param VOLUME_CAPACITY=4Gi --param MYSQL_VERSION=5.7
+
+sleep 15
+
+# oc new-app -f etherpad-template.yaml -p DB_USER=ether -p DB_PASS=ether -p DB_DBID=ether -p DB_PORT=3306 -p DB_HOST=mysql -p ADMIN_PASSWORD=secret
+oc new-app -f https://raw.githubusercontent.com/wkulhanek/docker-openshift-etherpad/master/etherpad-template.yaml -p DB_USER=ether -p DB_PASS=ether -p DB_DBID=ether -p DB_PORT=3306 -p DB_HOST=mysql -p ADMIN_PASSWORD=secret
